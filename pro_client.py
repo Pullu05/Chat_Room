eaa8987ad26c45055b4e668724d4f9ec6999fe12
Rishav Dutta <92175535+Pullu05@users.noreply.github.com>
2022-07-06 00:13:05 +0530

    Update pro_client.py

--- pro_client.py
+++ pro_client.py
@@ -4,21 +4,16 @@ import _thread
 import time
 
 
-# initialize server connection
 def initialize_client():
-    
-    #Create stream socket object
     s = socket(AF_INET, SOCK_STREAM)
 
     global name
     host = input(str("Enter server address: "))
     name = input(str("\nEnter your name: "))
     port = 1234
-
+    
     print("\nTrying to connect to ", host, "(", port, ")\n")
     time.sleep(1)
-
-    #Connect socket
     s.connect((host, port))
     print("Connected...\n")
 
@@ -30,30 +25,24 @@ def initialize_client():
     
     return s
 
-# update the chat log
+
 def update_chat(msg, state):
-    
     chatlog.config(state=NORMAL)
-    # update the message in the window
     if state==0:
         chatlog.insert(END, 'Me: ' + msg)
     else:
         chatlog.insert(END, s_name+': ' + msg)
     chatlog.config(state=DISABLED)
-    # show the latest messages
     chatlog.yview(END)
 
-# function to send message
+
 def send():
-    # get the message
     msg = textbox.get("0.0", END)
-    # update the chatlog
     update_chat(msg, 0)
-    # send the message
     s.send(msg.encode('ascii'))
     textbox.delete("0.0", END)
 
-# function to receive message
+
 def receive():
     while 1:
         try:
@@ -67,46 +56,30 @@ def receive():
 def press(event):
     send()
 
-# GUI function
 def GUI():
     global chatlog
     global textbox 
-
-    # initialize tkinter object
     gui = Tk()
-
-    # set title for the window
     gui.title(name)
 
-    # set size for the window
     gui.geometry("380x430")
-
-    # text space to display messages
     chatlog = Text(gui, bg='#C6CBDD')
     chatlog.config(state=DISABLED)
-
-    # button to send messages
+    
     sendbutton = Button(gui, bg='black', fg='white', text='SEND', command=send)
 
-    # textbox to type messages
     textbox = Text(gui, bg='white')
-
-    # place the components in the window
+    
     chatlog.place(x=6, y=6, height=386, width=370)
     textbox.place(x=6, y=401, height=20, width=265)
     sendbutton.place(x=300, y=401, height=20, width=50)
-    
-    # bind textbox to use ENTER Key
+
     textbox.bind("<KeyRelease-Return>", press)
 
-    # create thread to capture messages continuously
     _thread.start_new_thread(receive, ())
-
-    # to keep the window in loop
     gui.mainloop()
 
 
 if __name__ == '__main__': 
-    #chatlog = textbox = None
     s = initialize_client()
     GUI()
